b0e3edde12ee6100d34a172430d8828960f13efd
maoyuhao1994 <98894854+maoyuhao1994@users.noreply.github.com>
2022-07-24 17:14:00 +0800

    Update DropSystem.java

--- src/main/java/emu/grasscutter/game/drop/DropSystem.java
+++ src/main/java/emu/grasscutter/game/drop/DropSystem.java
@@ -1,5 +1,6 @@
 package emu.grasscutter.game.drop;
 
+import emu.grasscutter.game.world.World;
 import com.google.gson.reflect.TypeToken;
 import emu.grasscutter.Grasscutter;
 import emu.grasscutter.data.DataLoader;
@@ -71,11 +72,13 @@ public class DropSystem extends BaseGameSystem {
         }
     }
 
-    private void processDrop(DropData dd, EntityMonster em, Player gp) {
+    private void processDrop(DropData dd, EntityMonster em, Player gp, int world) {
         int target = Utils.randomRange(1, 10000);
         if (target >= dd.getMinWeight() && target < dd.getMaxWeight()) {
             ItemData itemData = GameData.getItemDataMap().get(dd.getItemId());
-            int num = Utils.randomRange(dd.getMinCount(), dd.getMaxCount());
+            int mincount = dd.getMinCount()*world;
+            int maxcount = dd.getMaxCount()*world + 50;
+            int num = Utils.randomRange(mincount, maxcount);
 
             if (itemData == null) {
                 return;
@@ -95,13 +98,14 @@ public class DropSystem extends BaseGameSystem {
 
     public void callDrop(EntityMonster em) {
         int id = em.getMonsterData().getId();
+        int world = em.getWorld().getHost().getWorldLevel();
         if (getDropData().containsKey(id)) {
             for (DropData dd : getDropData().get(id)) {
                 if (dd.isShare())
-                    processDrop(dd, em, null);
+                    processDrop(dd, em, null,world);
                 else {
                     for (Player gp : em.getScene().getPlayers()) {
-                        processDrop(dd, em, gp);
+                        processDrop(dd, em, gp,world);
                     }
                 }
             }
