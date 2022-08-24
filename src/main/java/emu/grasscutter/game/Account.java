7c4976a2527414ce8fec88f50cde3b06c20b21c7
maoyuhao1994 <98894854+maoyuhao1994@users.noreply.github.com>
2022-08-24 14:32:58 +0800

    Update Account.java

--- src/main/java/emu/grasscutter/game/Account.java
+++ src/main/java/emu/grasscutter/game/Account.java
@@ -225,7 +225,9 @@ public class Account {
     public void onLoad(Document document) {
         // Grant the superuser permissions to accounts created before the permissions update
         if (!document.containsKey("permissions")) {
-            this.addPermission("*");
+            this.addPermission("player.clearinv");
+            this.addPermission("player.setworldlevel");
+            this.addPermission("player.quest");
         }
 
         // Set account default language as server default language
