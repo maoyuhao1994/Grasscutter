e1c2201f16a3512a811915d1b2779b765d886511
maoyuhao1994 <98894854+maoyuhao1994@users.noreply.github.com>
2022-08-26 16:07:55 +0800

    Update DropSystem.java

--- src/main/java/emu/grasscutter/game/drop/DropSystem.java
+++ src/main/java/emu/grasscutter/game/drop/DropSystem.java
@@ -72,13 +72,12 @@ public class DropSystem extends BaseGameSystem {
         int target = Utils.randomRange(1, 10000);
         if (target >= dd.getMinWeight() && target < dd.getMaxWeight()) {
             ItemData itemData = GameData.getItemDataMap().get(dd.getItemId());
+            int mincount = dd.getMinCount()+world*2;
+            int maxcount = dd.getMaxCount()+world*2;
             if (dd.getItemId() == 201 || dd.getItemId() == 202 || dd.getItemId() == 101 || dd.getItemId() == 102){
                 int mincount = (int)(dd.getMinCount()*(1+world*0.5));
                 int maxcount = (int)(dd.getMaxCount()*(1+world*0.5));
-            } else {
-                int mincount = dd.getMinCount()+world*2;
-                int maxcount = dd.getMaxCount()+world*2;
-            }
+            } 
             int num = Utils.randomRange(mincount, maxcount);
             
             if (itemData == null) {
