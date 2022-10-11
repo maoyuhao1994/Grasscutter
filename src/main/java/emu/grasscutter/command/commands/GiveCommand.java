05905cf9f2ba43e20876a8b3423661746b9a6a25
maoyuhao1994 <98894854+maoyuhao1994@users.noreply.github.com>
2022-10-11 14:24:14 +0800

    Update GiveCommand.java

--- src/main/java/emu/grasscutter/command/commands/GiveCommand.java
+++ src/main/java/emu/grasscutter/command/commands/GiveCommand.java
@@ -31,8 +31,8 @@ import static emu.grasscutter.command.CommandHelpers.*;
     usage = {
         "(<itemId>|<avatarId>|all|weapons|mats|avatars) [lv<level>] [r<refinement>] [x<amount>] [c<constellation>]",
         "<artifactId> [lv<level>] [x<amount>] [<mainPropId>] [<appendPropId>[,<times>]]..."},
-    permission = "player.give",
-    permissionTargeted = "player.give.others",
+    permission = "server.give",
+    permissionTargeted = "server.give.others",
     threading = true)
 public final class GiveCommand implements CommandHandler {
     private enum GiveAllType {
