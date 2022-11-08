d72b477d9cc5baf8d4f73012ee750b1c643edbe6
maoyuhao1994 <98894854+maoyuhao1994@users.noreply.github.com>
2022-11-08 15:36:03 +0800

    Update GameMainQuest.java

--- src/main/java/emu/grasscutter/game/quest/GameMainQuest.java
+++ src/main/java/emu/grasscutter/game/quest/GameMainQuest.java
@@ -167,7 +167,7 @@ public class GameMainQuest {
         boolean didRewind = false;
         for (GameQuest quest : sortedByOrder) {
             int i = sortedByOrder.indexOf(quest);
-            if ( (i+1) >= sortedByOrder.size()) {
+            if ( i == sortedByOrder.size()) {
                 didRewind = quest.rewind(null);
             } else {
                 didRewind = quest.rewind(null);
